40d4d50f888e52a8b7e090cb77f6536e604ea228
Miguel García Holgado <91050890+mgarciaholgado@users.noreply.github.com>
2022-02-14 08:39:23 +0100

    heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-app.use(express.static('.dist/out-tsc/'));
+app.use(express.static('.dist/proyecto-fronted/'));
 app.get('/*', function(req, res) {
- res.sendFile('index.html', {root: 'dist/out-tsc/'}
+ res.sendFile('index.html', {root: 'dist/proyecto-fronted/'}
  );
 });
 const port = 3000;
